6d95d34c8f91cecdb869e7bff50a8b6e3aa16751
Yuichi Tateno <hotchpotch@gmail.com>
2009-03-04 20:43:26 +0900

    ちゅうかん

--- chrome/content/common/05-SuffixArray.js
+++ chrome/content/common/05-SuffixArray.js
@@ -7,19 +7,30 @@ const EXPORT = ['SuffixArray'];
 
 var SuffixArray = function (string) {
     this.string = string;
-    this.defaultLength = 20;
+    this.lowerString = string.toLowerCase();
+    this.defaultLength = 255;
 }
 
 SuffixArray.prototype = {
     make: function SuffixArray_createSuffixArray() {
-        let str = this.string.toLowerCase();
+        let string = this.lowerString;
         let sary = [];
-        let si = 0;
-        for (let i = 0, len = (str.length * 3/4); i < len; i++) {
-            let s = str.substr(i, this.defaultLength);
-            if (s.indexOf("\n") == -1)
-            sary[si++] = [s, i];
+        let saryIndex = 0;
+        let str;
+        let index;
+        let dLen = this.defaultLength;
+        p.b(function() {
+        for (let i = 0, len = string.length; i < len; i++) {
+            str = string.substr(i, dLen);
+            index = str.indexOf("\n");
+            if (index != 0) {
+                if (index != -1)
+                    str = str.substr(0, index);
+                sary[saryIndex++] = [str, i];
+            }
         }
+        }, 'make index');
+        p.b(function() {
         sary.sort(function(a, b) {
             if (a[0] > b[0]) {
                 return 1;
@@ -28,29 +39,67 @@ SuffixArray.prototype = {
             }
             return 0;
         });
+        }, 'sort');
         this.sary = sary.map(function([_,i]) i);
     },
     set sary (sary) { this._sary = sary; this._len = sary.length },
     get sary () this._sary,
     get length () this._len,
-    finder: function SuffixArray_finder(word) {
+    search: function SuffixArray_search(word) {
         let wLen = word.length;
-        if (wLen == 0) throw StopIteration;
+        if (wLen == 0) return [];
         if (!this.sary) this.make();
 
-        word.toLowerCase();
-        let str = this.string;
+        word = word.toLowerCase();
+        let string = this.lowerString;
         let sary = this.sary;
         let len = this.length;
-        let low = 0;
-        let high = len - 1;
-        let index = 0;
+        let lastIndex = -1;
+        let index = parseInt(len / 2);
 
-        do {
-            if (str.substr(sary[index], wLen).indexOf(word) == 0)
-                yield sary[index];
-        } while (++index < len);
-        throw StopIteration;
+        let floor = Math.floor;
+        let ceil = Math.ceil;
+
+        let str;
+        let range = index;
+
+        while (lastIndex != index) {
+            lastIndex = index;
+            str = string.substr(sary[index], wLen);
+            if (word < str) {
+                range = floor(range / 2);
+                index = index - range;
+            } else if (word > str) {
+                range = ceil(range / 2);
+                index = index + range;
+            } else {
+                let res = [sary[index]];
+                let start = index;
+                while (string.substr(sary[--start], wLen) == word)
+                    res.unshift(sary[start]);
+                let end = index;
+                while (string.substr(sary[++end], wLen) == word)
+                    res.push(sary[end]);
+                res.sort(function(a, b) a - b);
+                return res;
+            }
+        }
+
+        return [];
     }
 }
-
+/*
+     while (low < high) {
+            int middle = low + (high-low)/2;
+            if (suffixes[middle].compareTo(p) >= 0) {
+                high = middle;
+            } else {
+                low = middle+1;
+            }
+        }
+        if (suffixes[high].startsWith(p)) {
+            return len - suffixes[high].length();
+        }
+        return -1;
+    }
+*/
--- tests/javascripts/suffixarray.test.js
+++ tests/javascripts/suffixarray.test.js
@@ -17,35 +17,37 @@ function tearDown() {
 
 function testSecondlife() {
     var data = utils.readFrom('data/secondlife.search.data', 'UTF-8');
+    // var data = utils.readFrom('data/naoya.search.data', 'UTF-8');
+    data = data.substr(0, data.length * 3/4);
     assert.isTrue(data.length);
     var sary, finder, indexes;
 
     p.b(function() {
         sary = new SuffixArray(data);
         sary.make();
-        finder = sary.finder('Ruby on Rails');
     }, 'create');
 
+    var word = 'rubyonrails';
     p.b(function() {
         // indexes = [i for (i in finder)];
-        indexes = [];
-        for (let i = 0;i < 3;i++) {
-            try {
-                indexes.push(finder.next());
-            } catch (e if e instanceof StopIteration) {
-            };
-        }
-    }, 'find');
+        indexes = sary.search(word);
+    }, 'search');
 
-    p(''+indexes);
-    p(indexes.map(function(index) sary.string.substr(index, 10)).join('/'));
+    p('match:'+indexes.length + ' / ' + indexes);
+    p(indexes.map(function(index) sary.string.substr(index-5, word.length+10)).join(" # "));
 }
+
 
 function testSuffixArray()
 {
     return;
-    let sary = new SuffixArray('foobarbazmumubax');
-    let finder = sary.finder('ba');
+    let str = <>map foo bar map baz substr mappp ppmap maa
+   Construction began in 1974 on the 19 story structure, with plans to name it after a construction materials company that was to call it home.[1] Construction ended in 1974 on the $16 million dollar project,[2] but the namesake suffered economic setbacks and did not move into the building, leaving it nameless.[1] The building opened in 1975, and Benjamin Franklin Federal Savings and Loan Association moved their headquarters to the building and were able to get the building named as the Benjamin Franklin Plaza.[1] 
+    </>.toString();
+    let sary = new SuffixArray(str);
+    p(''+sary.search('llion'));
+
+    return;
 
     let index;
     index = finder.next();
