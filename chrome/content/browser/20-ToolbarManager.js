d67f87144e30fb6aa74dd513e631b6fbbbd6a4af
nanto_vi <nanto@moon.email.ne.jp>
2009-03-11 17:54:47 +0900

    不要なファイル(ツールバー旧実装)の削除。

--- chrome/content/browser/20-ToolbarManager.js
+++
@@ -1,8 +0,0 @@
-function initToolbar() {
-    let view = new ToolbarView();
-    let toolbar = document.getElementById("hBookmarkToolbar");
-    toolbar.view = view;
-    toolbar.addEventListener("HBookmarkToolbarReady", view, false);
-}
-
-window.addEventListener("load", initToolbar, false);
