215e409f5d74a2ee633cfa77f43ee4282fefa74f
nanto_vi <nanto@moon.email.ne.jp>
2009-03-12 16:50:33 +0900

    string bundle用ユーティリティを追加。

---
+++ tests/javascripts/Strings.test.js
@@ -0,0 +1,15 @@
+let hBookmark;
+
+function warmUp() {
+    utils.include("btil.js");
+    let g = loadAutoloader("chrome://hatenabookmark/content/unknown.js");
+    hBookmark = g.hBookmark;
+}
+
+function testGet() {
+    let uriSpec = baseURL + "Strings.test.properties";
+    let strings = new hBookmark.Strings(uriSpec);
+    assert.equals("Hello, world!", strings.get("hello"));
+    assert.equals("Hello, Strings world!",
+                  strings.get("helloSomething", ["Strings"]));
+}
