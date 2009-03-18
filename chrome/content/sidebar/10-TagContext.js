682f7164e1b29fbff9b4a795ce285c742f409b13
nanto_vi <nanto@moon.email.ne.jp>
2009-03-18 12:49:57 +0900

    タグのコンテキストメニューからタグ編集ページへ移動できるように。

--- chrome/content/sidebar/10-TagContext.js
+++ chrome/content/sidebar/10-TagContext.js
@@ -1,14 +1,33 @@
 const EXPORT = ["TagContext"];
 
+function getURIFor(pageType) {
+    let base = "http://b.hatena.ne.jp/";
+    let userBase = base + User.user.name + "/";
+    switch (pageType.toLowerCase()) {
+    case "tags":
+        let tags = arguments[1];
+        return userBase + tags.map(encodeURIComponent).join("/") + "/";
+
+    case "edittag":
+        let tag = arguments[1];
+        return userBase + "tag?tag=" + encodeURIComponent(tag);
+    }
+}
+
 function TagContext() {
     this.tags = null;
 }
 
 extend(TagContext.prototype, {
+    strings: new Strings("chrome://hatenabookmark/locale/popups.properties"),
+
     build: function TC_build(popup) {
         let tags = document.popupNode.tags;
         if (!tags || !tags.length) return false;
         this.tags = tags.concat();
+        this.tag = tags[tags.length - 1];
+        document.getElementById("hBookmarkTagContext_editTag").label =
+            this.strings.get("tagContext.editTagLabel", [this.tag]);
         return true;
     },
 
@@ -18,10 +37,10 @@ extend(TagContext.prototype, {
         BrowserWindow.openLinks(urls, event);
     },
     openIn: function TC_opneIn(where) {
-        let url = "http://b.hatena.ne.jp/" + User.user.name +
-                  "/" + this.tags.map(encodeURIComponent).join("/");
+        let url = getURIFor("tags", this.tags);
         openUILinkIn(url, where);
     },
+    /*
     delete: function TC_delete() {
         let tag = this.tags.pop();
         alert('Not implemented');
@@ -29,5 +48,10 @@ extend(TagContext.prototype, {
     rename: function TC_rename() {
         let tag = this.tags.pop();
         alert('Not implemented');
+    },
+    */
+    editTag: function TC_editTag(event) {
+        let url = getURIFor("editTag", this.tag);
+        openUILink(url, event);
     }
 });
