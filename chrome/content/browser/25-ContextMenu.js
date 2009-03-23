677c47275c1e2efd9d12995c3b1c92f42f6bbc7f
Yuichi Tateno <hotchpotch@gmail.com>
2009-03-23 22:21:03 +0900

    ツールバーのボタン周り

---
+++ chrome/content/browser/25-ContextMenu.js
@@ -0,0 +1,27 @@
+
+const EXPORT = ['ContextMenu'];
+
+elementGetter(this, 'addentry', 'hBookmark-menu-addentry', document);
+elementGetter(this, 'addlink', 'hBookmark-menu-addlink', document);
+
+var ContextMenu = {
+    registerEventListeners: function() {
+        let contextMenu = document.getElementById("contentAreaContextMenu");
+        contextMenu.addEventListener('popupshowing', ContextMenu.contextMenuHandler, false);
+    },
+    unregisterEventListeners: function() {
+        // contextMenu.removeEventListenr('popupshowing', ContextMenu.contextMenuHandler, false);
+    },
+    contextMenuHandler: function(ev) {
+        if (gContextMenu.onLink) {
+            addentry.setAttribute('hidden', true);
+            addlink.removeAttribute('hidden');
+        } else {
+            addentry.removeAttribute('hidden');
+            addlink.setAttribute('hidden', true);
+        }
+    },
+}
+
+window.addEventListener('load', ContextMenu.registerEventListeners, false)
+window.addEventListener('unload', ContextMenu.unregisterEventListeners, false);
