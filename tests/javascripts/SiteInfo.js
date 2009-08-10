bace70229ac618540506639ea0f47eb4a280ccde
nanto_vi <nanto@moon.email.ne.jp>
2009-08-10 11:31:08 +0900

    Revert "LDRize対応ページでのカウンタ表示。WIP。"
    This reverts commit b7806620ad16539d3ea1c061d619b3b5899ee4a2.

--- tests/javascripts/SiteInfo.js
+++ tests/javascripts/SiteInfo.js
@@ -29,36 +29,6 @@ function testSiteInfo() {
     assert.equals("dynamic result", info.query("dynamic"));
 }
 
-function testSiteInfo2() {
-    let set = new SiteInfoSet2({
-        matcher: SiteInfoSet2.createURLMatcher('url'),
-        sources: [
-            { data: [
-                {
-                    url: "^http://www\\.google(?:\\.\\w+){1,2}/",
-                    title: "Google",
-                },
-                {
-                    url: /^http:\/\/www\.yahoo\.co\.jp\//,
-                    title: "Yahoo",
-                    dynamic: function (context, defaultValue) "dynamic result",
-                },
-            ] },
-        ],
-    });
-
-    let info;
-
-    info = set.get(createDocumentForURL("http://www.google.co.jp/"));
-    assert.isTrue(info instanceof SiteInfo);
-    assert.equals("Google", info.data.title);
-
-    info = set.get(createDocumentForURL("http://www.yahoo.co.jp/path/to/something"));
-    assert.isTrue(info instanceof SiteInfo);
-    assert.equals("Yahoo", info.data.title);
-    assert.equals("dynamic result", info.query("dynamic"));
-}
-
 function createDocumentForURL(url) ({
     defaultView: {
         location: {
