707ae31fcaafc8bd4deeb9f4515836df2dc2e3c0
nanto_vi <nanto@moon.email.ne.jp>
2009-08-20 17:03:51 +0900

    「新しいタブで開く」が有効のときは、現在のタブで開くことがないように。

--- chrome/content/common/08-hOpenUILink.js
+++ chrome/content/common/08-hOpenUILink.js
@@ -1,17 +1,10 @@
 
 const EXPORT = ['hOpenUILink'];
 
-let key = (getService('@mozilla.org/xre/app-info;1', Ci.nsIXULRuntime).OS === 'Darwin')
-    ? 'metaKey' : 'ctrlKey';
-
 var hOpenUILink = function(link, ev) {
-    let event = ev;
-    if (Prefs.link.get('openInNewTab') && ev) {
-        event = {};
-        event[key] = !ev[key];
-        event.__proto__ = ev;
-    }
-    let where = whereToOpenLink(event);
+    let where = whereToOpenLink(ev);
+    if (where === 'current' && Prefs.link.get('openInNewTab'))
+        where = 'tab';
     if ((where === "tab" || where === "tabshifted") &&
         Prefs.bookmark.get("link.supportTreeStyleTab") &&
         typeof TreeStyleTabService !== "undefined")
